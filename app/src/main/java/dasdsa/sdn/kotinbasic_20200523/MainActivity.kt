7fc30b4e212b2cd308a15f874c98582ab2973e3f
umgonji <umgonji@naver.com>
2020-05-24 10:25:27 +0900

    버튼 누르면 => 텍스트뷰 내용 (text) 변경2

--- app/src/main/java/dasdsa/sdn/kotinbasic_20200523/MainActivity.kt
+++ app/src/main/java/dasdsa/sdn/kotinbasic_20200523/MainActivity.kt
@@ -3,6 +3,8 @@ package dasdsa.sdn.kotinbasic_20200523
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.InputEvent
+import android.view.inputmethod.InputContentInfo
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,8 +17,13 @@ class MainActivity : AppCompatActivity() {
 
 //            로그로 버튼 이벤트 확인
 //            Log.d("메인화면", "확인버튼눌림")
+//            Log.e("에러발생", "호ㅘ")
 
-            resultTxt.text = "버튼이 눌림"
+            val InputContent = inputEdt.text.toString()
+
+            resultTxt.text = InputContent
+
+//            Log.d(
 
         }
 
